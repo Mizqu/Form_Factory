95c47984a2837fa9d254768a924ccebdfdd365d3
Mizqu <mizqu64@gmail.com>
2024-04-17 12:02:12 +0200

    ModalToSearchForTrainerAdded

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
-import { Modal } from 'bootstrap';
+import { Component, OnInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'Frontend';
+  private TrainerModal: bootstrap.Modal | undefined;
+
   constructor() { }
 
+  ngOnInit(): void {
+    const modalElement = document.getElementById('FindTrainerModal');
+    if (modalElement) {
+      this.TrainerModal = new bootstrap.Modal(modalElement);
+      modalElement.addEventListener('hidden.bs.modal', () => {
+        this.clearModal();
+      });
+    }
+  }
 
   openModal(): void {
-    $('#FindTrainerModal').modal('show');
+    if (this.TrainerModal ) {
+      this.TrainerModal.show();
+    }
+  }
+
+  clearModal(): void {
+    if (this.TrainerModal) {
+      var NameInput = document.getElementById('Trainer_Name') as HTMLInputElement;
+      var CityInput = document.getElementById('City') as HTMLInputElement;
+      var FieldInput = document.getElementById('Field') as HTMLInputElement;
+      NameInput.value = '';
+      CityInput.value = '';
+      FieldInput.value = '';
+    }
   }
 }
+
